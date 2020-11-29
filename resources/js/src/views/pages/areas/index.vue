c30e83656f7f3c5b562739ff0b92efb9a162f723
slieman salah <slieman2001@hotmail.com>
2020-11-30 00:39:01 +0200

    solve bugs.

--- resources/js/src/views/pages/areas/index.vue
+++ resources/js/src/views/pages/areas/index.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>
-  test page
-  </div>
+    <div>
+      <b-table
+      :items="items"
+      :fields="fields"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      responsive="sm"
+    ></b-table>
+    </div>
 </template>
 
 <script>
 export default {
-
-}
+    data() {
+        return {
+          sortBy: 'id',
+        sortDesc: false,
+        fields: [
+          { key: 'id', label:'ID', sortable: true },
+          { key: 'country.name.ar', label:'Country', sortable: true },
+          { key: 'name.ar', label:'Name', sortable: true },
+          { key: 'status', label:'Status',  sortable: false }
+        ],
+        items: []
+        };
+    },
+    mounted() {
+         const requestSend = {
+                resource: "area",
+            };
+            this.$store.dispatch("areas/getData", requestSend).then(res => {
+                this.items = res.data.data;
+            });
+    }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
